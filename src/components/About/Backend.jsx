0d13a16399412c69dd10bac14346086a16b19a8b
leonardo <leonardoapelanda@gmail.com>
2023-01-26 16:32:01 -0300

    atualizado

--- src/components/About/Backend.jsx
+++ src/components/About/Backend.jsx
@@ -1,13 +1,8 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  DiJavascript1,
-  DiReact,
   DiNodejs,
   DiMongodb,
-  DiGit,
-  DiCss3,
-  DiHtml5
 } from "react-icons/di";
 import {
   SiMysql,
--- src/components/About/Cloud.jsx
+++ src/components/About/Cloud.jsx
@@ -1,20 +1,7 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
 import {
-  DiJavascript1,
-  DiReact,
-  DiNodejs,
-  DiMongodb,
-  DiGit,
-  DiCss3,
-  DiHtml5
-} from "react-icons/di";
-import {
-  SiMysql,
-  SiTypescript,
   SiAmazonaws,
-  SiExpress,
-  SiBootstrap,
 } from "react-icons/si";
 
 function Cloud() {
